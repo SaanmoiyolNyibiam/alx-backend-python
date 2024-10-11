f54dbd38e597beb970dfcb4407cd4c4a0a84878d
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-11 12:22:47 +0100

    build(alx-b-python:0x00): add solution to task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This is module that defines a function that concatenates strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This is a function that concatenates strings"""
+    return str1 + str2
