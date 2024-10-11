4a96609a2044097840264f3454d9ee477964a397
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-11 13:24:28 +0100

    build(alx-b-python:0x00): add solution to task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This is a module that defines a to_str function"""
+
+
+def to_str(n: float) -> str:
+    """This is function that converts a float to a string"""
+    return str(n)
