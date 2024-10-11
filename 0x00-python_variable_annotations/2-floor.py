4e5e254efc6fb6b99ad76b5289986d96918e0e4d
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-11 13:03:16 +0100

    build(alx-b-python:0x00): add solution to task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This is a module that defines a floor function"""
+import math
+
+
+def floor(f_input: float) -> int:
+    """This is a function that returns a floor of a float"""
+    return math.floor(f_input)
