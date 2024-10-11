e81d9ff682e27875559b395c6ab6710aa0451a23
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-11 22:06:38 +0100

    build(alx-b-python:0x00): add solution to task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This is a module that defines a sum_mixed_list function"""
+from typing import List, Union
+
+
+def sum_mixed_list(mixed_list: List[Union[int , float]]) -> float:
+    """sums up a list of mixed types and return a float"""
+    sum_total: float = 0
+    for value in mixed_list:
+        sum_total += value
+    return sum_total
