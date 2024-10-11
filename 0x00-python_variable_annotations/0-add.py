225912b523d63041bc58116c4f8dbae8f2703cf0
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-11 12:12:18 +0100

    build(alx-b-python:0x00): add solution to task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This is a module that defines an add function"""
+
+
+def add(a: float, b: float) -> float:
+    """This is a function that adds to foat values"""
+    return a + b
