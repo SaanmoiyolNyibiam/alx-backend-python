cfc86ea084cc70697e7110bf441e6fce1c9d5c92
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-14 15:57:50 +0100

    refactor(alx-b-python:0x01): change annotation for wait_random input to int

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,8 +4,8 @@ from random import uniform
 import asyncio
 
 
-async def wait_random(max_delay: float = 10) -> float:
+async def wait_random(max_delay: int = 10) -> float:
     """This is a function that waits for a random delay and returns it"""
-    delay_time: float = uniform(0, max_delay)
+    delay_time = uniform(0, max_delay)
     await asyncio.sleep(delay_time)
     return delay_time
