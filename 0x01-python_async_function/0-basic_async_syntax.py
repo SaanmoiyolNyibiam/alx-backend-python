978668712b6511c81cab83f9a6488d68b2ea65b5
ValentineNyibiam <nyibiamsaanmoiyol@gmail.com>
2024-10-14 15:24:42 +0100

    docs/build(alx-b-python:0x01): add readme file for 0x01-python_async_function directory and add solution to task 1

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""This is a module that defines a wait_random function"""
+from random import uniform
+import asyncio
+
+
+async def wait_random(max_delay: float = 10) -> float:
+    """This is a function that waits for a random delay and returns it"""
+    delay_time: float = uniform(0, max_delay)
+    await asyncio.sleep(delay_time)
+    return delay_time
